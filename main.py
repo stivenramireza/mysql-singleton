eb9b2364de699e3d17dd58db1903300622dcc980
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-12-24 09:08:25 -0500

    MySQL Singleton

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from src.secrets import (
 from src.singleton import MySQLConnection
 from src.logger import logger
 
-if __name__ == "__main__":
-   try:
+def main() -> None:
+    try:
        conn_mysql = MySQLConnection.get_instance(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT)
        logger.info(f'Connected to MySQL database successfully: {conn_mysql}')
-   except Exception as error:
+    except Exception as error:
        logger.error(f'Error to connect to MySQL database: {error}')
+
+if __name__ == "__main__":
+   main()
