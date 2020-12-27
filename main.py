5cf7c45abb6eeb7c8591aa16cf3957167c40e3fd
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-12-27 14:49:43 -0500

    Singleton finished

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+from src.secrets import (
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+    DB_PORT
+)
+
 from src.singleton import MySQLConnection
 from src.logger import logger
 
+import time
+
 def main() -> None:
     try:
-       conn_mysql = MySQLConnection.get_instance()
-       logger.info(f'Connected to MySQL database successfully: {conn_mysql}')
+       credentials = {
+          "DB_HOST": DB_HOST,
+          "DB_USER": DB_USER,
+          "DB_PASS": DB_PASS,
+          "DB_NAME": DB_NAME,
+          "DB_PORT": DB_PORT
+       }
+       conn_mysql = MySQLConnection.get_instance(credentials)
+       logger.info(f'MySQL database connected successfully: {conn_mysql}')
+       time.sleep(10)
+       MySQLConnection.close_instance()
+       logger.info(f'MySQL database closed successfully: {conn_mysql}')
     except TypeError as error:
        logger.error(f'Error to connect to MySQL database: {error}')
 
--- src/singleton.py
+++ src/singleton.py
@@ -1,25 +1,28 @@
-from src.secrets import (
-    DB_HOST,
-    DB_USER,
-    DB_PASS,
-    DB_NAME,
-    DB_PORT
-)
+from typing import Dict
 
 import mysql.connector as mysql
 
 class MySQLConnection:
 
-    __instance = mysql.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASS, database=DB_NAME, port=DB_PORT)
+    __instance = None
 
-    def __init__(self):
+    def __init__(self, host: str, user: str, password: str, name: str, port: str) -> None:
+        self.host = host
+        self.user = user
+        self.password = password
+        self.name = name
+        self.port = port
         if MySQLConnection.__instance is None:
-            MySQLConnection.__instance = self
+            MySQLConnection.__instance = mysql.connect(host=self.host, user=self.user, passwd=self.password, database=self.name, port=self.port)
         else:
             raise Exception('You cannot create another MySQL connection')
 
     @staticmethod
-    def get_instance():
+    def get_instance(credentials: Dict[str, str]) -> object:
         if not MySQLConnection.__instance:
-            MySQLConnection()
+            MySQLConnection(credentials['DB_HOST'], credentials['DB_USER'], credentials['DB_PASS'], credentials['DB_NAME'], credentials['DB_PORT'])
         return MySQLConnection.__instance
+
+    @staticmethod
+    def close_instance() -> None:
+        MySQLConnection.__instance.close()
